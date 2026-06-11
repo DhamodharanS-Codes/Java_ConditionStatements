7621751b9fc485754ab3fab2cf84ff59ece5de19
Dhamodharan S <dhamodharans2001@gmail.com>
2026-06-11 13:27:12 +0530

    New file is added with condition program

---
+++ ConditionStatements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Electricity_Bill {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter used units: ");
+        int unit= sc.nextInt();
+        int rate;
+        int finalBill;
+
+        if(unit<=100){
+            rate=0;
+        } else if (unit <=200) {
+            rate=1;
+
+        } else if (unit<=400) {
+            rate=2;
+
+        }
+        else{
+            rate=4;
+        }
+        finalBill=unit*rate;
+        System.out.println("Units you used this month: "+unit + "units");
+        System.out.println("Your bill amount is: "+finalBill);
+
+    }
+
+}
